b297b7834cfbddc5cb2da713929f63b334af6361
Max Bruce <maxbruce@student-10-201-04-186.wireless.adelaide.edu.au>
2022-08-09 16:25:09 +0930

    cheer

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 void print_class(std::string courses[4], std::string students[], int report_card[][4], int nstudents)
 {
-    cout << "Report Card";
+    cout << "Report Card ";
     for(int i=0; i<4; i++)
     {
-        cout << " " << courses[i] << " ";
+        cout << courses[i] << " ";
     }
     cout << endl; 
 
     for(int j=0; j<nstudents; j++)
     {
         cout << students[j] << " ";
-        for(int p=0; p<4; p++)
+        for(int p=0; p<3; p++)
         {
             cout << report_card[j][p] << " ";
         }
-        // cout << report_card[j][3];
+        cout << report_card[j][3];
         cout << endl;
         
     }
